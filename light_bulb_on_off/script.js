e730abaaf0fbb199d31cc1e543b60501cbeb411a
dinesh <dineshsutharinfo@gmail.com>
2024-08-18 20:34:31 +0530

    light_bulb_on_off -initial push

---
+++ light_bulb_on_off/script.js
@@ -0,0 +1,14 @@
+const switchBtn = document.querySelector(".switch");
+
+const body = document.body;
+
+switchBtn.addEventListener("click", () =>{
+    body.classList.toggle("on")
+})
+
+
+
+
+// switchBtn.onclick = function(){
+//     body.classList.toggle("on")
+// }
